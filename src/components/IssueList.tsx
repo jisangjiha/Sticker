937b6ec64abf01ed31eb4de034ac9cda21c4ea90
Jisang Hong <jisang1024@gmail.com>
2024-06-20 15:19:49 +0900

    Add list page

---
+++ src/components/IssueList.tsx
@@ -0,0 +1,29 @@
+import { Tooltip, Container, Box } from '@chakra-ui/react';
+
+import OpenIcon from '../icons/Open';
+
+export default function IssueList() {
+  return (
+    <Tooltip label="content">
+      <Container
+        border="1px"
+        borderRadius="lg"
+        padding="8px 16px"
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Box w="40px" display="flex" justifyContent="center">
+          <OpenIcon />
+        </Box>
+        <Box w="100%" padding="8px">
+          <Box>타이틀</Box>
+          <Box>작성자/작성일/#</Box>
+        </Box>
+        <Box w="40px" textAlign="center">
+          댓글
+        </Box>
+      </Container>
+    </Tooltip>
+  );
+}
--- src/page/GithubList.tsx
+++ src/page/GithubList.tsx
@@ -1,29 +1,9 @@
-import { Tooltip, Container, Box } from '@chakra-ui/react';
-
-import OpenIcon from '../icons/Open';
+import IssueList from '../components/IssueList';
 
 export default function GithubList() {
   return (
-    <Tooltip label="content">
-      <Container
-        border="1px"
-        borderRadius="lg"
-        padding="8px 16px"
-        display="flex"
-        justifyContent="space-between"
-        alignItems="center"
-      >
-        <Box w="40px" display="flex" justifyContent="center">
-          <OpenIcon />
-        </Box>
-        <Box w="100%" padding="8px">
-          <Box>타이틀</Box>
-          <Box>작성자/작성일/#</Box>
-        </Box>
-        <Box w="40px" textAlign="center">
-          댓글
-        </Box>
-      </Container>
-    </Tooltip>
+    <>
+      <IssueList />
+    </>
   );
 }
